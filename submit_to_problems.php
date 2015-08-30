a96a3df1a17181c3b54ad4f9c055643a63f7d010
bbtown <bll168@gwmail.gwu.edu>
2015-08-30 00:03:37 +0000

    Form test

--- submit_to_problems.php
+++ submit_to_problems.php
@@ -1,6 +1,6 @@
 <?php
 
-  require_once('rdb/rdb.php');
+  require_once('./vendor/danielmewes/php-rql/src/rdb/rdb.php');
   $conn = r\connect('52.20.101.105');
   $result = r\db("web")->table('problems')->run($conn);
   print_r($result);
