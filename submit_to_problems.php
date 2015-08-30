d82b3430e477de75a2b6e43302308c50fb1a836e
bbtown <bll168@gwmail.gwu.edu>
2015-08-30 00:26:58 +0000

    Tim change.

--- submit_to_problems.php
+++ submit_to_problems.php
@@ -1,6 +1,6 @@
 <?php
 
-  require_once __DIR__.'vendor/autoload.php';
+  require_once __DIR__.'/vendor/autoload.php';
   $conn = r\connect('52.20.101.105');
   $result = r\db("web")->table('problems')->run($conn);
   print_r($result);
