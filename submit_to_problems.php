1bd1ecf09934a9e283e81d7773ea3b632619be27
bbtown <bll168@gwmail.gwu.edu>
2015-08-30 01:39:55 +0000

    Tim change.

--- submit_to_problems.php
+++ submit_to_problems.php
@@ -1,13 +1,24 @@
 <?php
 
+  // load rethinkdb php library
   require_once __DIR__.'/vendor/autoload.php';
+
   $conn = r\connect('52.20.101.105');
   // $result = r\db("web")->table('problems')->run($conn);
   //foreach ($result as $doc) {
   //  print_r($doc);
   //}
 
-  print_r($_POST);
-  
+  $prob_desc = $_POST['prob_desc']; 
+  $prob_more = $_POST['prob_more']; 
+  $perspective = $_POST['perspective'];  
+  $url = $_POST['url'];
 
+  $document = Array('ProblemDescription' => $prob_desc,
+                    'MoreDescription' => $prob_more,
+                    'Perspective' => $perspective,
+                    'URL' => $url);  
+
+  $result = r\db("web")->table('problems')->insert($document)->run($conn);
+  echo "inserted data.";
 ?>
