680b37fbf155e5badc5a4cd51c4d638009d81ca7
bbtown <bll168@gwmail.gwu.edu>
2015-09-07 23:08:33 +0000

    minor updates

--- TestFeed.php
+++ TestFeed.php
@@ -86,22 +86,20 @@ $.ajax({
                      echo "<h4>Problem ID: </h4> ".$doc['id'];
 
 
+
+
                      echo "
-                     <script type \= 'text/javascript'>
+                     <script type = 'text/javascript'>
 
 
-<form class\=\"form-horizontal\" method\=\"post\" id=\"retrieve_id\" action\=\"retrieve_idea\.php\"> 
-<button onclick\=\"retrieve_id\(\)\;\" class\=\"btn btn-sm btn-primary m-t-n-xs\" style\=\"width:40%\" type\=\"button\"><strong>View Details</strong></button>
+<form class="form-horizontal" method="post" id="retrieve_id" action="retrieve_idea.php"> 
+<button onclick="retrieve_id();" class="btn btn-sm btn-primary m-t-n-xs" type="button"><strong>View Details</strong></button>
 </form>
-</script>
-";   
+</script>";
 
-
-                                                 } 
-                             }
-
-                             ?>
-
+}
+}
+?>
 
         </div>
         </div>
