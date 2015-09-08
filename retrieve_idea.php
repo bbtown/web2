6f2161524fdddfb753268503c1484df15d3de77c
bbtown <bll168@gwmail.gwu.edu>
2015-09-08 20:59:55 +0000

    experiment

--- retrieve_idea.php
+++ retrieve_idea.php
@@ -7,11 +7,11 @@
 
   $retrieve_id = $_POST['retrieve_id'];
 
-  $result = r\db("web")->table('ideas')->run($conn);
+//  $result = r\db("web")->table('ideas')->run($conn);
 
 
 
-//$result = r\db("web")->table('ideas')->get(id)->run($conn);
+$result = r\db("web")->table('ideas')->get(id)->run($conn);
 
 //$result = r\table("tablePhpTest")->count()->run($conn);
 
