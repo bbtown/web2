86c9fe4d887fa0e59e6a8ee14f414799eca18103
bbtown <bll168@gwmail.gwu.edu>
2015-09-13 04:22:31 +0000

    working on comments.

--- retrieve_idea.php
+++ retrieve_idea.php
@@ -1,17 +1,19 @@
 <?php
 
+
+
   // load rethinkdb php library
   require_once __DIR__.'/vendor/autoload.php';
 
   $conn = r\connect('52.20.101.105');
 
-  $retrieve_id = $_POST['retrieve_id'];
+  $retrieve_id = $_POST['idea_identifier'];
 
 //  $result = r\db("web")->table('ideas')->run($conn);
 
 
 
-//$result = r\db("web")->table('ideas')->getAll("$retrieve_id", array("index" => "id"))->run($conn);
+$result = r\db("web")->table('ideas')->getAll($retrieve_id, array("index" => "id"))->run($conn);
 
 
 //$result = r\db("web")->table('ideas')->count()->run($conn);
@@ -19,9 +21,9 @@
 
 
 
-    $result = r\db("web")->table('ideas')->map(function($x) {
-            return $x('id');
-        })->run($conn);
+    //$result = r\db("web")->table('ideas')->map(function($x) {
+    //        return $x('id');
+    //    })->run($conn);
 
 //    foreach ($result as $doc) {
 //        print_r($doc);
@@ -62,13 +64,14 @@
 //		    'URL' => $url);  
 
 
-$document = Array ('id' => $retrieve_id);
+//$document = Array ('id' => $retrieve_id);
 
     foreach ($result as $doc) {
        print_r($doc);
-if ($retrieve_id == $doc['id']){ // forgot single quote 11:42 BL 
-      print_r($doc);
+//if ($retrieve_id == $doc['id']){ // forgot single quote 11:42 BL 
+      //print_r($doc);
     }}
+
 
 
 ?>
