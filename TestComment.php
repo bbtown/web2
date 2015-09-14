a0e7bac2afff2af9cf26aa1ce522b0c57f9b8ec3
bbtown <bll168@gwmail.gwu.edu>
2015-09-14 18:14:32 +0000

    trying to find buttons

--- TestComment.php
+++ TestComment.php
@@ -350,7 +350,10 @@
                      echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-heart\"></i> Love</a>";
                      echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-pencil\"></i> Edit</a>";
                      echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-trash\"></i> Delete</a>";
-                     echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-delete\"></i> Delete</a>";
+                     echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-add\"></i> Add</a>";
+                     echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-plus\"></i> Plus</a>";
+                     echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-minus\"></i> Minus</a>";
+
                      echo "</div></div></div>";
 
 
