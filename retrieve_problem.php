d3fcb544eba946d864f3203048bca4017addac49
bbtown <bll168@gwmail.gwu.edu>
2015-09-23 03:33:32 +0000

    retrying

--- retrieve_problem.php
+++ retrieve_problem.php
@@ -67,19 +67,250 @@ var retrieve_id = $('#retrieve').val();
 </head>
 
 <body>
+      <div id="wrapper">
+    <nav class="navbar-default navbar-static-side" role="navigation">
 
-<div class="ibox-content">
-                          <!--  <form method="get" class="form-horizontal"> -->
+      <div style="position: right overflow: hidden; width: 300px; height: 100%;" class="sidebar-collapse">
+            
+            
+                  <div class="ibox-content">
+                    <div class="row">
+<h1>Other Problems</h1>
+                               
+                                <h4>Sub-problems: </h4>
+                                <p><ul><li>Polar Ice Caps Melting</li> 
+                                    <li>Erratic Weather Patterns</li> 
+                                    <li>Collapse of Ecosystems</li></ul></p>
+                                    
+                                    <h4>Related Problems: </h4>
+                                    <p><ul><li>Lack of Recycling Programs</li>
+                                        <li>Lack of Viable Alternate Fuel Sources</li></ul></p>
 
-        <div class="row"> 
-       <div class="col-sm-9">
+                               </div></div>
+<!--        BEGIN COMMENT BOX -->
+
+<!--    <div class="row m-t-lg">
+            <div class="col-lg-6">
+                <div class="ibox float-e-margins">
+-->
+                    <div class="ibox-content">
+
+                        <div class ="row"> 
+                <div class="chat-activity-list">
+
+                            <?php
+                             require_once __DIR__.'/vendor/autoload.php';
+                             $conn = r\connect('52.20.101.105');
+                             $result = r\db("web")->table('comments')->run($conn);
+                             foreach ($result as $doc) {
+                               if(isset($doc['CommentText'])) {
+                                 echo "<div class=\"chat-element\">";
+                                 echo "<a href=\"#\" class=\"pull-left\">";
+                                echo "<img alt=\"image\" class=\"img-circle\" src=\"img/a2.jpg\">";
+                                 echo "</a>";
+                                 echo "<div class=\"media-body \">";
+                                // echo "<small class=\"pull-right text-navy\">1m ago</small>";
+                     echo "<strong>".$doc['CommentUser']."</strong>";
+                     echo "<p class=\"m-b-xs\">".$doc['CommentText'];
+                     echo "</p>";
+                     echo "<small class=\"text-muted\">".$doc['CommentTS']."</small>";
+                     echo "<div class=\"actions\">";
+                     echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-thumbs-up\"></i> Like </a>";
+                     echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-thumbs-down\"></i> Dislike </a>";
+            echo "</div></div></div>";
+                             }
+                             }
+                             ?>
+</div></div>
+     <!--    <div class="row m-t-lg">
+           <div class="col-lg-6">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-content">
+                        <div> -->
+               <!-- <div class="chat-activity-list">
+
+                    <div class="chat-element">
+                        <a href="#" class="pull-left">
+                            <img alt="image" class="img-circle" src="img/a2.jpg">
+                        </a>
+                        <div class="media-body ">
+                            <small class="pull-right text-navy">1m ago</small>
+                            <strong>Mike Smith</strong>
+                            <p class="m-b-xs">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                            </p>
+                            <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
+                        </div>
+                    </div>
+                </div> -->
+                        <div class="chat-form">
+                            <form role="form" class="form-horizontal" method="post" name="comment_form" id="comment_form" action="submit_problem_comment.php">
+                                <div class="form-group">
+                                    <textarea class="form-control" placeholder="Comment" maxlength="500" name="comment_txt" id="comment_txt"></textarea>
+                                
+                                <div class="text-right">
+                                    <button onclick="add_comment();" class="btn btn-sm btn-primary m-t-n-xs" style="width:40%" type="button"><strong>Post Comment</strong></button>
+                                </div></div>
+                            </form>
+                        </div>
+                </div>
+                </div>
+             </div>  
+                    </nav>
+                  </div>
+
+        <div id="page-wrapper" class="gray-bg">
+        <div class="row border-bottom">
+        <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+                <form role="search" class="navbar-form-custom" action="search_results.html">
+                    <div class="form-group">
+                        <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
+                    </div>
+                </form>
+            </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+                    <span class="m-r-sm text-muted welcome-message">Welcome to INSPINIA+ Admin Theme.</span>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                        <i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-messages">
+                        <li>
+                            <div class="dropdown-messages-box">
+                                <a href="profile.html" class="pull-left">
+                                    <img alt="image" class="img-circle" src="img/a7.jpg">
+                                </a>
+                                <div class="media-body">
+                                    <small class="pull-right">46h ago</small>
+                                    <strong>Mike Loreipsum</strong> started following <strong>Monica Smith</strong>. <br>
+                                    <small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="dropdown-messages-box">
+                                <a href="profile.html" class="pull-left">
+                                    <img alt="image" class="img-circle" src="img/a4.jpg">
+                                </a>
+                                <div class="media-body ">
+                                    <small class="pull-right text-navy">5h ago</small>
+                                    <strong>Chris Johnatan Overtunk</strong> started following <strong>Monica Smith</strong>. <br>
+                                    <small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="dropdown-messages-box">
+                                <a href="profile.html" class="pull-left">
+                                    <img alt="image" class="img-circle" src="img/profile.jpg">
+                                </a>
+                                <div class="media-body ">
+                                    <small class="pull-right">23h ago</small>
+                                    <strong>Monica Smith</strong> love <strong>Kim Smith</strong>. <br>
+                                    <small class="text-muted">2 days ago at 2:30 am - 11.06.2014</small>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="text-center link-block">
+                                <a href="mailbox.html">
+                                    <i class="fa fa-envelope"></i> <strong>Read All Messages</strong>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                        <i class="fa fa-bell"></i>  <span class="label label-primary">8</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-alerts">
+                        <li>
+                            <a href="mailbox.html">
+                                <div>
+                                    <i class="fa fa-envelope fa-fw"></i> You have 16 messages
+                                    <span class="pull-right text-muted small">4 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="profile.html">
+                                <div>
+                                    <i class="fa fa-twitter fa-fw"></i> 3 New Followers
+                                    <span class="pull-right text-muted small">12 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="grid_options.html">
+                                <div>
+                                    <i class="fa fa-upload fa-fw"></i> Server Rebooted
+                                    <span class="pull-right text-muted small">4 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="text-center link-block">
+                                <a href="notifications.html">
+                                    <strong>See All Alerts</strong>
+                                    <i class="fa fa-angle-right"></i>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+
+                <li>
+                    <a href="login.html">
+                        <i class="fa fa-sign-out"></i> Log out
+                    </a>
+                </li>
+            </ul>
+
+        </nav>
+        </div>
+            <div class="row wrapper border-bottom white-bg page-heading">
+                <div class="col-lg-10">
+                    <h2>Panels</h2>
+                    <ol class="breadcrumb">
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>
+                            <a>UI Elements</a>
+                        </li>
+                        <li class="active">
+                            <strong>Panels</strong>
+                        </li>
+                    </ol>
+                </div>
+                <div class="col-lg-2">
+
+                </div>
+            </div>
+        <div class="wrapper wrapper-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-title">
 <h1>Problem Description</h1>
-
+<div class="ibox-content">
 <?php
   // load rethinkdb php library
-  require_once __DIR__.'/vendor/autoload.php';
-  $conn = r\connect('52.20.101.105');
-  $retrieve_id = $_POST['problem_identifier'];
+ // require_once __DIR__.'/vendor/autoload.php';
+//  $conn = r\connect('52.20.101.105');
+//  $retrieve_id = $_POST['problem_identifier'];
 //  $result = r\db("web")->table('ideas')->run($conn);
 $result = r\db("web")->table('problems')->getAll($retrieve_id, array("index" => "id"))->run($conn);
     //$result = r\db("web")->table('ideas')->map(function($x) {
@@ -120,10 +351,10 @@ echo "<h4>Tags: ".$doc['Tags']."</h4>";
 
 
 
-
+<!--
                                      <div class="col-sm-3  gray-bg">    
                                 <div class="row">
-				             <h1>Other Problems</h1>
+				           <h1>Other Problems</h1>
                                 <h4>Sub-problems: </h4>
                                 <p><ul><li>Polar Ice Caps Melting</li> 
                                     <li>Erratic Weather Patterns</li> 
@@ -133,48 +364,22 @@ echo "<h4>Tags: ".$doc['Tags']."</h4>";
                                     <p><ul><li>Lack of Recycling Programs</li>
                                         <li>Lack of Viable Alternate Fuel Sources</li></ul></p>
 
-				                               </div>
+				                           </div> -->
 <!--        BEGIN COMMENT BOX -->
 
 <!--    <div class="row m-t-lg">
             <div class="col-lg-6">
                 <div class="ibox float-e-margins">
 -->
-                    <div class="ibox-content">
 
-                        <div class ="row"> 
-                <div class="chat-activity-list">
-
-                            <?php
-                             require_once __DIR__.'/vendor/autoload.php';
-                             $conn = r\connect('52.20.101.105');
-                             $result = r\db("web")->table('comments')->run($conn);
-                             foreach ($result as $doc) {
-                               if(isset($doc['CommentText'])) {
-                                 echo "<div class=\"chat-element\">";
-                                 echo "<a href=\"#\" class=\"pull-left\">";
-                                echo "<img alt=\"image\" class=\"img-circle\" src=\"img/a2.jpg\">";
-                                 echo "</a>";
-                                 echo "<div class=\"media-body \">";
-                                // echo "<small class=\"pull-right text-navy\">1m ago</small>";
-                     echo "<strong>".$doc['CommentUser']."</strong>";
-                     echo "<p class=\"m-b-xs\">".$doc['CommentText'];
-                     echo "</p>";
-                     echo "<small class=\"text-muted\">".$doc['CommentTS']."</small>";
-                     echo "<div class=\"actions\">";
-                     echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-thumbs-up\"></i> Like </a>";
-                     echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-thumbs-down\"></i> Dislike </a>";
-				        echo "</div></div></div>";
-                             }
-                             }
-                             ?>
-</div></div>
+</div></div></div></div></div>
         <div class="row m-t-lg">
 <!--            <div class="col-lg-6">
                 <div class="ibox float-e-margins">
                     <div class="ibox-content">
                         <div> -->
                <!-- <div class="chat-activity-list">
+
                     <div class="chat-element">
                         <a href="#" class="pull-left">
                             <img alt="image" class="img-circle" src="img/a2.jpg">
@@ -189,7 +394,7 @@ echo "<h4>Tags: ".$doc['Tags']."</h4>";
                         </div>
                     </div>
                 </div> -->
-                        <div class="chat-form">
+<!--                        <div class="chat-form">
                             <form role="form" class="form-horizontal" method="post" name="comment_form" id="comment_form" action="submit_problem_comment.php">
                                 <div class="form-group">
                                     <textarea class="form-control" placeholder="Comment" maxlength="500" name="comment_txt" id="comment_txt"></textarea>
@@ -202,6 +407,7 @@ echo "<h4>Tags: ".$doc['Tags']."</h4>";
                 </div>
                 </div>
              </div>  
+-->
 <!--   BEGiN FORM STUFF -->
 
  <div class="ibox-content">
@@ -811,6 +1017,8 @@ else {echo "<h4>Impact Type: </h4>";};
                         </ol>
                       
                     </div>
+                    </div>
+                </div>
                     </div>
                 </div>
     <!-- Mainly scripts -->
