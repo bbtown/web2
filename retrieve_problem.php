e28b44805913ebf3c5b44527b98f5983432d9f5b
bbtown <bll168@gwmail.gwu.edu>
2015-09-29 22:44:30 +0000

    experiment

--- retrieve_problem.php
+++ retrieve_problem.php
@@ -71,15 +71,15 @@ var retrieve_id = $('#retrieve').val();
 
 <!--    <nav class="navbar-default navbar-static-side" role="navigation"> -->
 <!--<div class="fixed-sidebar"> -->
-<nav class="navbar navbar-static-right col-sm-3" role="navigation">
-      <div  class="slimScrollDiv" style="position: relative overflow: hidden; width: auto; height: 100%;">
+<nav class="navbar navbar-static-right" role="navigation">
+      <div  class="slimScrollDiv" style="position: right overflow: hidden; width: auto; height: 100%;">
      <div class="sidebar-collapse" style="overflow: hidden; width: auto; height: 100%;">
             
             <ul class="nav metismenu" id="side-menu">
                 <li class="nav-header">
 
-                  <div class="ibox-content" style="margin:0"> 
-                    <div class="row">
+                  <div class="ibox-content" style="margin:0; padding:0"> 
+                    <div class="row" style="margin:0; padding:0">
 <h1>Other Problems</h1>
                                
                                 <h4>Sub-problems: </h4>
