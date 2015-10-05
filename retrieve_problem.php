103c5eafb52808e62aaa26aee84afb064e7e54b5
bbtown <bll168@gwmail.gwu.edu>
2015-10-05 20:13:23 +0000

    messing with formatting

--- retrieve_problem.php
+++ retrieve_problem.php
@@ -83,7 +83,7 @@
     <nav class="navbar-default navbar-static-side" role="navigation">
 
       <div class="slimScrollDiv" style="position: relative overflow: hidden; width: auto; height: 100%;"> 
-     <!--<div class="sidebar-collapse" style="overflow: hidden; width: 100%; height: 100%;"> -->
+     <div class="sidebar-collapse" style="overflow: hidden; width: 100%; height: 100%;"> 
             
           <!--  <ul class="nav metismenu" id="side-menu">
                 <li class="nav-header">
