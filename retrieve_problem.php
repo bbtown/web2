7ebccd8e039fad9331906ef83007d43ffe158ed9
bbtown <bll168@gwmail.gwu.edu>
2015-10-06 00:32:37 +0000

    sidebar experiment

--- retrieve_problem.php
+++ retrieve_problem.php
@@ -84,7 +84,7 @@
                  <div id="wrapper bg-white col-lg-3">
 <!--    <nav class="navbar-default navbar-static-side rtls" role="navigation"> -->
 
-<nav class="navbar navbar-static-side" role="navigation">
+<nav class="navbar-default navbar-static-side">
 
 
 
@@ -214,10 +214,10 @@
                         </div></li>
                         </ul>
                 </div></div>
-               <!-- </div>
-            </div>-->
+               
+          <!--   </div>-->
                     </nav>
-                 
+                 </div>
                 <div style="background: rgb(0, 0, 0) none repeat scroll 0% 0%; width: 7px; position: absolute; top: 0px; opacity: 0.4; display: none; border-radius: 7px; z-index: 99; right: 1px; height: 78.0259px;" class="slimScrollBar"></div>
 <div style="width: 7px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; background: rgb(51, 51, 51) none repeat scroll 0% 0%; opacity: 0.9; z-index: 90; right: 1px;" class="slimScrollRail"></div>
                   </div> 
