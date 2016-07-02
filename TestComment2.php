9b58244b6c8e1fa3c12f3d398ce7919a66ecdbed
bbtown <bll168@gwmail.gwu.edu>
2016-07-02 02:10:31 +0000

    sidebar experiment

--- TestComment2.php
+++ TestComment2.php
@@ -193,7 +193,7 @@ echo "
           echo "<textarea class=\"form-control\" placeholder=\"Beth\" maxlength=\"500\" name=\"vote_user\" id=\"vote_user\"></textarea>";
 
 
-   echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-heart\" onClick=\"upvote();\"></i> Love</a>";
+   echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-heart\" onclick=\"upvote();\"></i> Love</a>";
 echo "</form>";
    echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-pencil\"></i> Edit</a>";
    echo "<a class=\"btn btn-xs btn-white\"><i class=\"fa fa-trash\"></i> Delete</a>";
