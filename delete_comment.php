d7c02b9a2159d88590e2869b0e983f3764de4210
bbtown <bll168@gwmail.gwu.edu>
2016-07-08 07:48:27 +0000

    experimenting with delete

--- delete_comment.php
+++ delete_comment.php
@@ -15,7 +15,7 @@
 //$comment_tags = $_POST['comment_tags'];
 $comment_status = "delete";
 //$comment_sentiment = $_POST['comment_sentiment'];
-$comment_ts = gmdate("l jS \of F Y h:i:s A");
+//$comment_ts = gmdate("l jS \of F Y h:i:s A");
 //$flag_count = $_POST['flag_count'];
 //$vote_down_count = $_POST['vote_down_count'];
 //$vote_up_count = $_POST['vote_up_count'];
@@ -29,17 +29,17 @@ $comment_ts = gmdate("l jS \of F Y h:i:s A");
 
 $comment_user = "<-WhoIsThisGuy";
 
-  $document = Array('CommentText' => $comment_txt,
-  	   	  'CommentIdeaID' => $comment_idea_id,
-		  'CommentProblemID' => $comment_prob_id,
-		  'CommentTags' => $comment_tags,
-		  'CommentStatus' => $comment_status,
-		  'CommentSentiment' => $comment_sentiment,
-		  'CommentTS' => $comment_ts,
-		  'CommentUser' => $comment_user,
-		  'FlagCount' => $flag_count,
-		  'VoteDownCount' => $vote_down_count,
-		  'VoteUpCount' => $vote_up_count);
+  $document = Array('CommentStatus' => $comment_status,
+  //	   	  'CommentIdeaID' => $comment_idea_id,
+//		  'CommentProblemID' => $comment_prob_id,
+//		  'CommentTags' => $comment_tags,
+		  'CommentText' => $comment_txt,
+//		  'CommentSentiment' => $comment_sentiment,
+//		  'CommentTS' => $comment_ts,
+//		  'CommentUser' => $comment_user,
+//		  'FlagCount' => $flag_count,
+//		  'VoteDownCount' => $vote_down_count,
+//		  'VoteUpCount' => $vote_up_count);
 
   $result = r\db("web")->table('comments')->insert($document)->run($conn);
   echo "Thank you for posting your comment!";
