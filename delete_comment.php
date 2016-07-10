ebeaa0502f943e223121844391cf087fe9489213
bbtown <bll168@gwmail.gwu.edu>
2016-07-10 06:57:29 +0000

    experimenting with delete comment

--- delete_comment.php
+++ delete_comment.php
@@ -14,10 +14,10 @@ $comment_status = comment_status;
 $deleted_ts = gmdate("l jS \of F Y h:i:s A");
 $deleted_by = deleted_by;
 
-  $document = Array('CommentStatus' => $comment_status,
-		  'DeletedTS' => $deleted_ts,
-		  'DeletedbBy' => $deleted_by);
+//  $document = Array('CommentStatus' => $comment_status,
+//		  'DeletedTS' => $deleted_ts,
+//		  'DeletedbBy' => $deleted_by);
 
-  $result = r\db("web")->table('comments')->get($comment_id)->insert(array('DeletedTS' => $deleted_ts, 'DeletedBy' => $deleted_by))->run($conn);
+  $result = r\db("web")->table('comments')->get($comment_id)->update(array('CommentStatus' => $comment_status, 'DeletedTS' => $deleted_ts, 'DeletedBy' => $deleted_by))->run($conn);
   echo "You have deleted your comment!";
 ?>
