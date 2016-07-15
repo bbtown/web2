469737925e26f23f1b270c853aaaaaa9adea0ac9
bbtown <bll168@gwmail.gwu.edu>
2016-07-15 03:30:27 +0000

    jquery

--- PetitionFeed.php
+++ PetitionFeed.php
@@ -140,7 +140,7 @@ $.ajax({
             </div>
             </div>
     <!-- Mainly scripts -->
-    <script src="js/jquery-3.1.0.js"></script>
+    <script src="js/jquery-2.1.1.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
     <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
