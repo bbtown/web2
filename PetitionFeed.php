fa0fcb249b7b7571e2c6b4fc42cb6792b5c6601b
bbtown <bll168@gwmail.gwu.edu>
2016-07-19 22:42:25 +0000

    experimenting with show/hide

--- PetitionFeed.php
+++ PetitionFeed.php
@@ -95,7 +95,7 @@ $.ajax({
 					  	 			         <br/><br/><br/>
 												        <button input type=\"submit\" class=\"btn btn-sm btn-primary m-t-n-xs\" name='petition_identifier' value=\"$retrieve_id\" style=\"width:40%\" type=\"button\"><strong>View/Edit Details</strong></button>
 														      		        </form>
-																	    ";
+</td>																	    ";
                     echo "<td>".print_r($doc['issues'][0]['name'])."</td>";
                     //echo "<td>".$doc['Tags']."</td>";
                     echo "<td>...</td>";
