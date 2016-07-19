f4d1c56c2e0907d2f2fbe1b3a17ce2a63478c19a
bbtown <bll168@gwmail.gwu.edu>
2016-07-19 21:09:24 +0000

    experimenting with show/hide

--- TestComment4.php
+++ TestComment4.php
@@ -185,7 +185,7 @@
      echo "<strong>".$doc['CommentUser']."</strong>";
      echo "<p class=\"m-b-xs\">".$doc['CommentText'];
      echo "</p>";
-     $time = moment($doc['CommentTS'] * 1000);
+     $time = momenti.unix($doc['CommentTS'] * 1000);
      echo $time;
      echo "<small class=\"text-muted\">".$doc['CommentTS']."</small>";
      //echo "<small class=\"text-muted\">".$time.format()."</small>";
