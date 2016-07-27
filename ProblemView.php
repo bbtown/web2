35e49de6f8232ce55ae624162ee756f8d44e1086
bbtown <bll168@gwmail.gwu.edu>
2016-07-27 20:52:38 +0000

    pie chart example

--- ProblemView.php
+++ ProblemView.php
@@ -656,6 +656,12 @@
     <script src="js/plugins/flot/jquery.flot.resize.js"></script>
     <script src="js/plugins/flot/jquery.flot.pie.js"></script>
 
+
+    <!-- ChartJS-->
+    <script src="js/plugins/chartJs/Chart.min.js"></script>
+    <script src="js/demo/chartjs-demo.js"></script>
+
+
     <script>
          $(document).ready(function(){
 
