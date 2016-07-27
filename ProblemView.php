611a8b5eeb2dab6dd5e7d7d37251f8bb5cb9776f
bbtown <bll168@gwmail.gwu.edu>
2016-07-27 21:37:51 +0000

    graph experiment:

--- ProblemView.php
+++ ProblemView.php
@@ -406,33 +406,47 @@
                     </div>
                 </div>
 
+
+
+</div>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+
+
         <div class="wrapper wrapper-content animated fadeInRight">
             <div class="row">
-
-
                 <div class="col-lg-6">
                     <div class="ibox float-e-margins">
                         <div class="ibox-title">
-                            <h5>Pie </h5>
-
+                            <h5>Line Chart Example
+                                <small>With custom colors.</small>
+                            </h5>
                             <div ibox-tools></div>
                         </div>
                         <div class="ibox-content">
                             <div>
-                                <canvas id="doughnutChart" height="140"></canvas>
+                                <canvas id="lineChart" height="140"></canvas>
                             </div>
                         </div>
                     </div>
                 </div>
-</div>
-</div>
-
-</div>
-                    </div>
-                    </div>
+                <div class="col-lg-6">
+                    <div class="ibox float-e-margins">
+                        <div class="ibox-title">
+                            <h5>Bar Chart Example</h5>
+                            <div ibox-tools></div>
+                        </div>
+                        <div class="ibox-content">
+                            <div>
+                                <canvas id="barChart" height="140"></canvas>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
+            </div>
+        </div>
                 <div class="ibox-content">
                     <div class="row">
                         <div class="col-sm-12">
