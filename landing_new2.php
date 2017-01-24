d13e15914ddeabd42c0aa4d8888c3da2d80c2cda
bbtown <bll168@gwmail.gwu.edu>
2017-01-24 23:50:46 +0000

    trying to clean up button

--- landing_new2.php
+++ landing_new2.php
@@ -203,10 +203,10 @@ $.ajax({
 			  $count++;
 			  $counter++;
                     if($counter <13){
-		    echo "<div class=\"col-md-4\" style=\"max-height: 5;\">";
-                    echo "<div class=\"ibox-content product-box\" style=\"min-height:5;\">";
+		    echo "<div class=\"col-md-4\">";
+                    echo "<div class=\"ibox-content product-box\">";
                     echo "<div class=\"product-imitation\"><i class=\"fa fa-heart big-icon\"></i></div>";
-                    echo "<div class=\"product-desc\">";
+                    echo "<div class=\"product-desc\" style=\"height:3;\">";
                           
                     if(isset($doc['ProblemCategory'])){echo "<span class=\"product-price\">".$doc['ProblemCategory']."</span>";};
                     echo "<div class=\"product-name\">".$doc['ProblemDescription']."</div>";
