c51899123e5c439919add5c520eb5e23a976b727
bbtown <bll168@gwmail.gwu.edu>
2017-01-25 01:34:47 +0000

    cleaning up

--- landing_new2.php
+++ landing_new2.php
@@ -169,8 +169,8 @@ $.ajax({
     <div class="container">
 
     <div class="wrapper wrapper-content">
-          <div class="col-lg-8" align="center">
-    <div class="row"> 
+          <div class="col-lg-12" align="center">
+    <div class="row" align="center" style="width:60%"> 
         <h1>Submit a Problem</h1>
 <div class="input-group"><input type="text" class="form-control"> <span class="input-group-btn"> <button type="button" class="btn btn-primary" style=
     "background-color: #4CAF50">Submit
