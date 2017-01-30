fd0139228a2273d5d8fc6992cbd28ff397ef7074
bbtown <bll168@gwmail.gwu.edu>
2017-01-30 00:43:18 +0000

    Restoring old mess

--- retrieve_problem.php
+++ retrieve_problem.php
@@ -87,6 +87,7 @@ CTYPE html>
                     <div class="row">
 <!--    <nav class="navbar-default navbar-static-side rtls" role="navigation"> -->
 
+<nav class="navbar navbar-static-side">
 
 
 
@@ -103,7 +104,35 @@ CTYPE html>
 <!--<nav class="navbar-default navbar-static-side col-lg-3" role="navigation">
         <div class="sidebar-collapse"> -->
    <ul class="nav metismenu" id="side-menu"> 
-
+               <!--  <li class="nav-header">
+                   <div class="dropdown profile-element"> <span>
+                            <img alt="image" class="img-circle" src="img/profile_small.jpg" />
+                             </span>
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
+                             </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
+                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                            <li><a href="profile.html">Profile</a></li>
+                            <li><a href="contacts.html">Contacts</a></li>
+                            <li><a href="mailbox.html">Mailbox</a></li>
+                            <li class="divider"></li>
+                            <li><a href="login.html">Logout</a></li>
+                        </ul>
+                    </div> 
+                    <div class="logo-element">
+                        IN+
+                    </div>
+                </li>
+                <li>
+                    <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="index.html">Dashboard v.1</a></li>
+                        <li><a href="dashboard_2.html">Dashboard v.2</a></li>
+                        <li><a href="dashboard_3.html">Dashboard v.3</a></li>
+                        <li><a href="dashboard_4_1.html">Dashboard v.4</a></li>
+                    </ul>
+                </li>
+            </ul>-->
 <h1>Other Problems</h1>
                                
                                 <h4>Sub-problems: </h4>
@@ -154,7 +183,26 @@ CTYPE html>
                              ?>
                            
 </div>
-
+     <!--    <div class="row m-t-lg">
+           <div class="col-lg-6">
+                <div class="ibox float-e-margins">
+                    <div class="ibox-content">
+                        <div> -->
+               <!-- <div class="chat-activity-list">
+                    <div class="chat-element">
+                        <a href="#" class="pull-left">
+                            <img alt="image" class="img-circle" src="img/a2.jpg">
+                        </a>
+                        <div class="media-body ">
+                            <small class="pull-right text-navy">1m ago</small>
+                            <strong>Mike Smith</strong>
+                            <p class="m-b-xs">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                            </p>
+                            <small class="text-muted">Today 4:21 pm - 12.06.2014</small>
+                        </div>
+                    </div>
+                </div> -->
 
                 <li>
                         <div class="chat-form">
@@ -180,7 +228,124 @@ CTYPE html>
 
      <div id="page-wrapper" class="gray-bg col-lg-9 ">
         <div class="row border-bottom">
-      
+        <nav class="navbar navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <a class="navbar-minimalize minimalize-styl-2 btn btn-primary " href="#"><i class="fa fa-bars"></i> </a>
+                <form role="search" class="navbar-form-custom" action="search_results.html">
+                    <div class="form-group">
+                        <input type="text" placeholder="Search for something..." class="form-control" name="top-search" id="top-search">
+                    </div>
+                </form>
+            </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li>
+                    <span class="m-r-sm text-muted welcome-message">Welcome to INSPINIA+ Admin Theme.</span>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                        <i class="fa fa-envelope"></i>  <span class="label label-warning">16</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-messages">
+                        <li>
+                            <div class="dropdown-messages-box">
+                                <a href="profile.html" class="pull-left">
+                                    <img alt="image" class="img-circle" src="img/a7.jpg">
+                                </a>
+                                <div class="media-body">
+                                    <small class="pull-right">46h ago</small>
+                                    <strong>Mike Loreipsum</strong> started following <strong>Monica Smith</strong>. <br>
+                                    <small class="text-muted">3 days ago at 7:58 pm - 10.06.2014</small>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="dropdown-messages-box">
+                                <a href="profile.html" class="pull-left">
+                                    <img alt="image" class="img-circle" src="img/a4.jpg">
+                                </a>
+                                <div class="media-body ">
+                                    <small class="pull-right text-navy">5h ago</small>
+                                    <strong>Chris Johnatan Overtunk</strong> started following <strong>Monica Smith</strong>. <br>
+                                    <small class="text-muted">Yesterday 1:21 pm - 11.06.2014</small>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="dropdown-messages-box">
+                                <a href="profile.html" class="pull-left">
+                                    <img alt="image" class="img-circle" src="img/profile.jpg">
+                                </a>
+                                <div class="media-body ">
+                                    <small class="pull-right">23h ago</small>
+                                    <strong>Monica Smith</strong> love <strong>Kim Smith</strong>. <br>
+                                    <small class="text-muted">2 days ago at 2:30 am - 11.06.2014</small>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="text-center link-block">
+                                <a href="mailbox.html">
+                                    <i class="fa fa-envelope"></i> <strong>Read All Messages</strong>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
+                        <i class="fa fa-bell"></i>  <span class="label label-primary">8</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-alerts">
+                        <li>
+                            <a href="mailbox.html">
+                                <div>
+                                    <i class="fa fa-envelope fa-fw"></i> You have 16 messages
+                                    <span class="pull-right text-muted small">4 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="profile.html">
+                                <div>
+                                    <i class="fa fa-twitter fa-fw"></i> 3 New Followers
+                                    <span class="pull-right text-muted small">12 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="grid_options.html">
+                                <div>
+                                    <i class="fa fa-upload fa-fw"></i> Server Rebooted
+                                    <span class="pull-right text-muted small">4 minutes ago</span>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <div class="text-center link-block">
+                                <a href="notifications.html">
+                                    <strong>See All Alerts</strong>
+                                    <i class="fa fa-angle-right"></i>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
+
+
+                <li>
+                    <a href="login.html">
+                        <i class="fa fa-sign-out"></i> Log out
+                    </a>
+                </li>
+            </ul>
+
+        </nav>
         </div>
             <div class="row wrapper border-bottom white-bg page-heading">
                 <div class="col-lg-7">
@@ -230,6 +395,27 @@ $('#username').editable();
                                title: 'Enter comments'
                             });
 
+
+<table><tbody>
+<tr>         
+                        <td>Wysihtml5 (now support bootstrap 3 !!!). <a href="#" id="pencil"><i class="icon-pencil" style="padding-right: 5px"></i>[edit]</a></td>
+                        <td>
+                          <div id="note" data-pk="1" data-type="wysihtml5" data-toggle="manual" data-title="Enter notes" data-placement="top">
+                            <h3>WYSIWYG</h3>
+                            WYSIWYG means <i>What You See Is What You Get</i>.<br>
+                            But may also refer to:
+                              <ul>
+                                <li>WYSIWYG (album), a 2000 album by Chumbawamba</li>
+                                <li>"Whatcha See is Whatcha Get", a 1971 song by The Dramatics</li>
+                                <li>WYSIWYG Film Festival, an annual Christian film festival</li>
+                              </ul>
+                              <i>Source:</i> <a href="http://en.wikipedia.org/wiki/WYSIWYG_%28disambiguation%29">wikipedia.org</a> 
+                            
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
 
 <h1>Problem Description</h1>
@@ -480,46 +666,50 @@ echo "<h4>Tags: ".$doc['Tags']."</h4>";
 
 
 
-  <div class="col-lg-9 gray-bg">
-      <h1>Who</h1>
-      <div class="row"> 
-      <div class="col-lg-9">
-        <div class="ibox collapsed">
-          <div class="ibox-title">
+                     <div class="col-lg-9 gray-bg">
+                            <h1>Who</h1>
+                              <div class="row"> 
+
+
+            <div class="col-lg-9">
+                <div class="ibox collapsed">
+                    <div class="ibox-title">
 <?php
 if(isset($doc['AffectedNumber'])) {
-echo "<div class="row"><h4>Impact Type: ".$doc['AffectedNumber']." affected</h4></div>";}
+echo "<h4>Impact Type: ".$doc['AffectedNumber']." affected</h4>";}
 else {echo "<h4>Impact Type: </h4>";};
 ?>
 
-      <div class="ibox-tools">
-          <a class="collapse-link">
-            Edit<i class="fa fa-pencil"></i>
-          </a>
-      </div>
-      </div>
-      <div class="ibox-content">
-         <div class='form-group'>
-          <div class="row">
-            <div class=col-sm-2><label class='control-label'>Describe the group of people affected by the problem.</label></div>
-              <div class='col-sm-7'>
-                <input type='text' id='affect_desc' name='affect_desc' class='form-control input-lg' placeholder='e.g., children attending public school in Pennsylvania'> 
-              </div>
-            </div>
-        </div>
+<div class="ibox-tools">
+                            <a class="collapse-link">
+                                Edit<i class="fa fa-pencil"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="ibox-content">
 
-        <div class='form-group'>
-          <label class='col-sm-2 control-label'>Estimate the number of people directly affected by this problem.</label>
-            <div class='col-sm-5'>
-              <div class='input-group'>
-                  <input type='text' id='affect_num' class='form-control' placeholder='e.g., 7,290,000,000' name='affect_num' maxlength='14'>
-                  <span class='help-block m-b-none'>Number must be between 1 and 7.5 billion</span> 
-              </div>
+
+                                <div class='form-group'>
+                                  <label class='col-sm-2 control-label'>Describe the group of people affected by the problem.</label>
+                                    <div class='col-sm-10'>
+                                      <input type='text' id='affect_desc' name='affect_desc' class='form-control input-lg' placeholder='e.g., children attending public school in Pennsylvania'> 
+                                    </div>
+                                </div>
+
+                                <div class='form-group'>
+                                    <label class='col-sm-2 control-label'>Estimate the number of people directly affected by this problem.</label>
+                                    <div class='col-sm-5'>
+                                        <div class='input-group'>
+                                        <input type='text' id='affect_num' class='form-control' placeholder='e.g., 7,290,000,000' name='affect_num' maxlength='14'>
+                                       <span class='help-block m-b-none'>Number must be between 1 and 7.5 billion</span> 
+                                      </div>
+                                    </div>
+
+
+</div>
+                </div>
             </div>
-        </div>
-      </div>
-  </div>
-  </div>                             
+        </div>                             
 
 <div class="hr-line-dashed"></div>                                
                         <div class="row">
@@ -1036,7 +1226,7 @@ else {echo "<h4>Impact Type: </h4>";};
 
 
 
-  <!--bootstrap 3 -->
+	<!--bootstrap 3 -->
         <link href="bootstrap3-editable-1.5.1/bootstrap3-editable/css/bootstrap.css" rel="stylesheet">
         <script src="bootstrap3-editable-1.5.1/bootstrap3-editable/js/bootstrap.js"></script>
     
